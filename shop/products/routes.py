dc0f44d9815d42f13ca1b1ae88a4cd6275a7ac25
dkotsilo <thetrackorg@gmail.com>
2021-03-02 13:33:58 +0300

    work 2 march

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, url_for, flash, request, session
+from flask import render_template, redirect, url_for, flash, request, session, current_app
 from shop import db, app, photos
 from .models import Brand, Category, Addproduct
 from .forms import Addproducts
 import secrets
+import os
 
 @app.route('/addbrand', methods=['GET','POST'])
 def addbrand():
@@ -65,33 +66,6 @@ def updatecat(id):
     return render_template('products/addproduct.html', title='Update category page', updatecat=updatecat)
 
 
-# @app.route('/addproduct', methods=['GET','POST'])
-# def addproduct():
-#     if 'email' not in session:
-#         flash('Please login first', 'danger')
-#         return redirect(url_for('login'))
-#     brands = Brand.query.all()
-#     categories = Category.query.all()
-#     form = Addproducts(request.form)
-#     if request.method == "POST":
-#         name = form.name.data
-#         price = form.price.data
-#         discount = form.discount.data
-#         stock = form.stock.data
-#         colors = form.colors.data
-#         desc = form.description.data
-#         brand = request.form.get('brand')
-#         category = request.form.get('category')
-#         image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")  ## the library secrets is for give random name to images
-#         image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
-#         image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
-#         addpro = Addproduct(name=name, price=price, discount=discount, stock=stock, colors=colors, desc=desc,
-#                              brand_id=brand, category_id=category, image_1=image_1, image_2=image_2, image_3=image_3)
-#         db.session.add(addpro)
-#         flash(f'The product {name} has been added to your database', 'success')
-#         db.session.commit()
-#         return redirect(url_for('addproduct'))
-#     return render_template('products/addproduct.html', title='Add Product page', form=form, brands=brands, categories=categories)
 
 @app.route('/addproduct', methods=['GET','POST'])
 def addproduct():
@@ -134,6 +108,24 @@ def updateproduct(id):
         product.category_id = category
         product.colors = form.colors.data
         product.desc = form.description.data
+        if request.files.get('image_1'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_1))
+                product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+        if request.files.get('image_2'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_2))
+                product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+        if request.files.get('image_3'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images/" + product.image_3))
+                product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
+            except:
+                product.image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
         db.session.commit()
         flash(f'Yor product {product.name} has been updated', 'success')
         return redirect(url_for('admin'))
