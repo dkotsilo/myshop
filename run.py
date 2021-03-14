8bbacd510456ad3b93037bcdf654f4822ad47db6
dkotsilo <thetrackorg@gmail.com>
2021-03-14 21:27:28 +0300

    home 26 12_3

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 from shop import app
-if __name__ =="__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -21,8 +21,7 @@ def AddCart():
         product = Addproduct.query.filter_by(id=product_id).first()
         if product_id and quantity and product and request.method == "POST":
             DictItems = {product_id: {'name': product.name, 'price': product.price, 'discount': product.discount,
-                                      'color': color, 'quantity': quantity, 'image': product.image_1}}
-
+                                      'color': color, 'quantity': quantity, 'image': product.image_1, 'colors': product.colors}}
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
                 if product_id in session['Shoppingcart']:
@@ -45,4 +44,23 @@ def AddCart():
 def getCart():
     if 'Shoppingcart' not in session:
         return redirect(request.referrer)
-    return render_template('products/carts.html')
+    subtotal = 0
+    grandtotal = 0
+    for key, product in session['Shoppingcart'].items():
+        discount = float(product['discount'] / 100 ) * float(product['price'])
+        subtotal = float(product['price']) * int(product['quantity'])
+        subtotal -= discount
+        tax = ("%.2f" % (.06 * float(subtotal)))
+        grandtotal = float("%.2f" % (1.06 * subtotal))
+
+    return render_template('products/carts.html', tax=tax, grandtotal=grandtotal)
+
+
+
+@app.route('/empty')
+def empty_cart():
+    try:
+        session.clear()
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
